96c3da7b2b096b36144ffd63f5dbdcb44f64ec9a
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2026-07-22 01:14:17 -0700

    working on poll time
    users will be able to "poll" for a time for their event

--- schema/event.py
+++ schema/event.py
@@ -3,7 +3,7 @@ from typing import Any, TYPE_CHECKING
 
 from google.api_core.exceptions import InvalidArgument
 from pydantic import field_validator, computed_field, model_validator
-from sqlalchemy import Column
+from sqlalchemy import Column, ARRAY
 from sqlmodel import Field, Relationship, SQLModel
 
 from schema.links import UserEventRSVPLink
@@ -29,6 +29,7 @@ class EventCreate(SQLModel):
 class Event(EventCreate, table=True):
     id: int | None = Field(default=None, primary_key=True)
     group_id: int = Field(foreign_key="group.id")
+    name:str = Field(index=True)
     time_range: tuple[time, time] = Field(sa_column=Column(TimeRangeType))
     created_at: datetime = Field(default_factory=datetime.now)
 
@@ -37,6 +38,8 @@ class Event(EventCreate, table=True):
         back_populates="rsvp_events",
         link_model=UserEventRSVPLink,
     )
+    poll_times: list[tuple[time, time]] = Field(sa_column=Column(ARRAY(TimeRangeType)))
+    best_poll_time:tuple[time, time] = Field(default=time_range, sa_column=Column(TimeRangeType))
 
     @field_validator("time_range", mode="before")
     @classmethod
@@ -58,6 +61,39 @@ class Event(EventCreate, table=True):
             return False
         self.rsvp_users.remove(user)
         return True
+
+    def add_poll_time(self, user: "User", time: "TimeRangeType") -> bool:
+        if len(self.poll_times) == 0 or len(self.poll_times) != len(self.rsvp_users) + 1:
+            return False
+        if user in self.rsvp_users:
+            #add one to the rsvp users index because the creator's suggested time is always first
+            index:int = self.rsvp_users.index(user) + 1
+            self.poll_times[index] = (time[0], time[1])
+            return True
+        self.rsvp_users.append(user)
+        self.poll_times.append((time[0], time[1]))
+        return True
+
+
+    def remove_poll_time(self, user: "User", time: "TimeRangeType") -> bool:
+        if len(self.poll_times) == 0 or len(self.poll_times) != len(self.rsvp_users) + 1:
+            return False
+        if user in self.rsvp_users:
+            #add one to the rsvp users index because the creator's suggested time is always first
+            index:int = self.rsvp_users.index(user) + 1
+            #hacky but OK
+            del self.poll_times[index]
+            self.rsvp_users.remove(user)
+            return True
+        return False
+
+    def compute_best_poll_time(self):
+        intersect_list = [0] * 48
+        max_number = 0
+        #todo: finish this
+
+
+
 
 
 

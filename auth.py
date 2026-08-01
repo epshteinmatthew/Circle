10c266bb11e2cccf0a9153f516b404b226af1ccb
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2026-07-31 22:38:55 -0700

    we try in vain

--- auth.py
+++ auth.py
@@ -42,7 +42,7 @@ def validate_uid(encoded, uid:int):
     try:
         decoded = jwt.decode(encoded, setup.GOOGLE_CLIENT_SECRET, algorithms=["HS256"])
         print(decoded['uid'])
-        print(uid)
+        print(type(uid))
         print(decoded['uid'] == uid)
         if decoded['exp'] >= time.time() and decoded['cid'] == setup.GOOGLE_CLIENT_ID and decoded['uid'] == uid:
             return True

81aee67af41afe5269a9f5259144542492607841
epshteinmatthew <64236644+epshteinmatthew@users.noreply.github.com>
2026-07-31 22:39:34 -0700

    a javascript esque solution...

--- auth.py
+++ auth.py
@@ -44,7 +44,7 @@ def validate_uid(encoded, uid:int):
         print(decoded['uid'])
         print(type(uid))
         print(decoded['uid'] == uid)
-        if decoded['exp'] >= time.time() and decoded['cid'] == setup.GOOGLE_CLIENT_ID and decoded['uid'] == uid:
+        if decoded['exp'] >= time.time() and decoded['cid'] == setup.GOOGLE_CLIENT_ID and int(decoded['uid']) == int(uid):
             return True
         else:
             return False
